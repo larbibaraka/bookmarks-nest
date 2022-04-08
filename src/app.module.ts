10972082e804af686f694f4807d0d07e1317b0fb
larbibaraka <larbibaraka@gmail.com>
2022-04-09 00:04:20 +0100

    create three modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-
-
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [AuthModule, UserModule, BookmarkModule],
 })
 export class AppModule {}

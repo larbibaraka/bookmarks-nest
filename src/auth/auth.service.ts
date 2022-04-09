b6bb434fa13e35f4c81a51908dbd36a12a210265
larbibaraka <larbibaraka@gmail.com>
2022-04-10 00:20:24 +0100

    adding error management

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AuthDto } from 'src/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable({})
 export class AuthService {
@@ -13,15 +14,24 @@ export class AuthService {
   }
 
   async signup({ email, password }: AuthDto) {
-    const hash = await argon.hash(password);
-    const user = await this.prisma.user.create({
-      data: {
-        email,
-        hash,
-      }
-    });
+    try {
+      const hash = await argon.hash(password);
+      const user = await this.prisma.user.create({
+        data: {
+          email,
+          hash,
+        },
+      });
 
-    delete user.hash;
-    return user;
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+          if(error.code === 'P2002'){
+              throw new ForbiddenException('Credentials taken')
+          }
+      }
+      throw error;
+    }
   }
 }

ffe4d623b82aadf7c9f35dd8d2e97bbad4e96553
larbibaraka <larbibaraka@gmail.com>
2022-04-10 01:29:33 +0100

    Implement Jwt Strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { JwtModule } from '@nestjs/jwt';
  
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategy';
 
 @Module({
   imports : [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}

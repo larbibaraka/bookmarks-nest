2fa86b51dabdb4a0ba055338419124d3febb89bc
larbibaraka <larbibaraka@gmail.com>
2022-04-10 23:25:25 +0100

    adding a way to get specefic param

--- src/auth/decorator/get-user.decorator.ts
+++ src/auth/decorator/get-user.decorator.ts
@@ -1,9 +1,12 @@
-
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const GetUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const request : Express.Request = ctx.switchToHttp().getRequest();
+  (data: string | undefined, ctx: ExecutionContext) => {
+    const request: Express.Request = ctx.switchToHttp().getRequest();
+    // if any specification like i want to get only the id of user
+    if (data) {
+      return request.user[data];
+    }
     return request.user;
   },
 );

fd665c0cdcd8bd4b8b2f6e7dae73f8e25a266684
larbibaraka <larbibaraka@gmail.com>
2022-04-10 01:46:43 +0100

    adding validate method && return user infos

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,13 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(config: ConfigService) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService, private prisma: PrismaService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('jwt_secret'),
     });
+  }
+
+  async validate({ sub, email }: { sub: number; email: string }) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: sub,
+      },
+    });
+
+    delete user.hash;
+    return user;
   }
 }
